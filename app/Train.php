73b003d5a3bcef1bb828ef1cf880f82ffa1c2cc3
Angeloamenta <angeloamenta7@gmail.com>
2022-02-18 15:23:56 +0100

    creazione controller-model

---
+++ app/Train.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Train extends Model
+{
+//   protected $fillable = 
+//   [
+    
+//     'agency',
+//     'departure_station',
+//     'arrival_station',
+//     'departure_day',
+//     'departure_time',
+//     'arrival_time',
+//     'code',
+//     'carriages',
+//     'in_time',
+//     'deleted'
+    
+//   ];
+}
