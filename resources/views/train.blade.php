0828e240f6ca7c5f2e3eff7c3cd651bf99a3b1ba
Angeloamenta <angeloamenta7@gmail.com>
2022-02-21 16:20:10 +0100

    aggiunti elementi alla pagina train

--- resources/views/train.blade.php
+++ resources/views/train.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 <div class="train-card">
- {{$train->agency}}
+    <h1>{{$train->agency}}</h1>
+    <h2>Partenza da</h2>
+    <h4>{{$train->departure_station}}</h4>
+    <h2>Arrivo a</h2>
+    <h4>{{$train->arrival_station}}</h4>
+    <h2>Partenza</h2>
+    <h4>{{$train->departure_day}}</h4>
 </div>
 @endsection
